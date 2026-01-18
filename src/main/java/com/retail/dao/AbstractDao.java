f460147fbce25cfefb0d5c3bfeba2b420684e3ee
nurislam270408-ctrl <nurislam270408@gmail.com>
2026-01-18 23:30:00 +0500

    dao + db
    # Conflicts:
    #	src/main/java/com/retail/dao/AbstractDao.java
    #	src/main/java/com/retail/dao/BaseDao.java
    #	src/main/java/com/retail/dao/CategoryDao.java
    #	src/main/java/com/retail/dao/InventoryDao.java
    #	src/main/java/com/retail/dao/ProductDao.java
    #	src/main/java/com/retail/dao/ReceiptDao.java
    #	src/main/java/com/retail/dao/ReceiptItemDao.java
    #	src/main/java/com/retail/dao/SaleDao.java
    #	src/main/java/com/retail/dao/SaleItemDao.java
    #	src/main/java/com/retail/dao/SecurityLogDao.java
    #	src/main/java/com/retail/dao/StockLogDao.java
    #	src/main/java/com/retail/dao/UnitDao.java
    #	src/main/java/com/retail/dao/UserDao.java
    #	src/main/java/com/retail/db/DatabaseManager.java

--- src/main/java/com/retail/dao/AbstractDao.java
+++ src/main/java/com/retail/dao/AbstractDao.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-
 public abstract class AbstractDao<T, ID> implements BaseDao<T, ID> {
     protected final Logger logger = LoggerFactory.getLogger(getClass());
     protected final DatabaseManager dbManager;
